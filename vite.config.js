63413be3406ec094b7f307b14dd1512e287b5c9b
Subhodeep Basak <85038045+Subhodeep014@users.noreply.github.com>
2024-05-11 13:38:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/expense_app_react_deploy/",
+  base: "/react_expense_app_deploy/",
   plugins: [react()],
 })
